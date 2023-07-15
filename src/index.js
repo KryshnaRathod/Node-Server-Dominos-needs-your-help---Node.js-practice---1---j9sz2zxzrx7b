eae32cec1e19b7f696bc439c5c515d938a4829c5
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-15 12:02:04 +0530

    Updated  index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,35 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser())
+
+app.get("/welcome", (req, resp) => {
+    resp.status(200);
+    resp.setHeader('Content-Type', 'text/plain');
+    resp.send("Welcome to Dominos!")
+})
+
+app.get("/contact", function(req, resp){
+    resp.status(200);
+    resp.setHeader('Content-Type', 'application/json');
+    resp.send({
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+      });
+})
+
+app.use(function(req, res, next) {
+    res.status(404);
+    res.send('404');
+});
+
+const server = app.listen(8081, function(){
+    console.log("App running on %s", server.address().port)
+})
 
 function handleServer(req, res) {
   
